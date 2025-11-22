18cfb04ba024686b24627ad2b46fde7cd8ed5882
Alex Parlett <alexparlett@proton.me>
2025-11-22 10:56:12 +0000

    Remove old code

--- src/callfunc/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod call;
-pub mod context;
-pub mod registry;
-pub mod interface;
-pub mod wrappers;
--- src/vm/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod gc;
-pub mod memory;
-pub mod vm;
-
-// Re-export commonly used items
-pub use gc::{GCFlags, GCStatistics, GarbageCollector};
-pub use memory::{Object, ObjectHeap};
-pub use vm::{StackFrame, VM, VMState};
